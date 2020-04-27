565e35b315c2c9d8b91b222bf3990b4245cb0c23
EppurSiMu0ve <60028627+EppurSiMu0ve@users.noreply.github.com>
2020-04-27 12:31:58 -0300

    adicionado: remoção de configurações
    Agora o usuário pode remover apenas uma configuração predefinida selecionada ou todas.

--- fcsf-wpa.sh
+++ fcsf-wpa.sh
@@ -115,7 +115,10 @@ obtem_redes(){
 	REDES=()
 
 	# --------| Obtém as redes disponíveis para a interface selecionada $1
-	tempRedes=$(iw dev ${INTERFACES[$1]} scan -u | grep -i ssid | sed s/" * "/" "/ | cut -d" " -f2)
+	wpa_cli -i ${INTERFACES[$1]} <<< $'scan_interval 4\nscan' 1>& /dev/null
+	sleep 4
+	local tempRedes=$(wpa_cli -i ${INTERFACES[$1]} <<< $'scan_results\n' | grep WPA)
+	tempRedes=$(sed s/"\t"/" "/g <<< "$tempRedes" | cut -d" " -f5)
 
 	# --------| Sai se não houver redes disponíveis |--------------------
 	[[ -z $tempRedes ]] && echo "Não há redes sem-fio disponíveis para ${INTERFACES[$1]}..." && return 1
@@ -123,7 +126,7 @@ obtem_redes(){
 	# --------| Armazena cada rede num elemento da array ''REDES'' |-----
 	local i=-1
 	for tempRede in $tempRedes; do
-		REDES[$((++i))]=$tempRede
+		REDES[$((++i))]="$tempRede"
 	done
 	return 0
 }
@@ -176,12 +179,12 @@ define_rede(){
 
 		# Itera as redes disponíveis para escrever no menu
 		for tempRede in ${REDES[@]}; do
-			escr_item "> $((++i)) : $tempRede"
+			escr_item "> [ $((++i)) ] : $tempRede"
 		done
 
 		# variável que armazena a rede escolhida
-		numRede=-1
-		while [[ 10#${numRede/*[a-zA-Z]*/-1} -gt $(( ${#REDES[@]} - 1 )) || 10#${numRede/*[a-zA-Z]*/-1} -lt 0 ]]
+		local numRede=-1
+		while [[ 10#${numRede/*[a-zA-Z]*/-1} -gt $i || 10#${numRede/*[a-zA-Z]*/-1} -lt 0 ]]
 		do
 			escr_item "> "
 			read numRede
@@ -259,30 +262,41 @@ carrega_predefinicoes(){
 # ------| MOSTRA MENU COM INTERFACES E REDES JÁ CONFIGURADAS |-------------------------
 menu_predefinicoes(){
 	escr_secao "ESCOLHER CONEXÃO PREDEFINIDA ( VAZIO = 0 )"
+	obtem_interfaces
 	local subsecao=''
 	local i
 
 	# itera array PREDEFINICOES
 	for ((i=0; i < ${#PREDEFINICOES[@]}; i=i+1)); do
-		def=${PREDEFINICOES[$i]}
 
 		# separa cada predefinição para mostrar no menu
-		iface=$(cut -d":" -f1 <<< $def)
-		rede=$(cut -d":" -f2 <<< $def)
+		local def=${PREDEFINICOES[$i]}
+		local rede_def=$(cut -d":" -f2 <<< $def)
+		local iface_def=$(cut -d":" -f1 <<< $def)
+		local nIface=''
+		for n in ${!INTERFACES[@]}; do
+			if [[ "${INTERFACES[$n]}" = "$iface_def" ]]; then
+				nIface=$n
+			fi
+		done
 
-		# só escreve uma nova subseção se for diferente da anterior (interfaces diferentes)
-		if [[ "$iface" != "$subsecao" ]]; then
+		# só escreve uma nova subseção e escaneia interface se for diferente da anterior
+		if [[ "$iface_def" != "$subsecao" ]]; then
 			escr_subsecao "Redes configuradas para $iface"
-			subsecao=$iface
+			subsecao="$iface_def"
+			obtem_redes "$nIface"
 		fi
 
-		# escreve rede atual para interface (seção) atual
-		escr_item "[ $i ]. $rede"
+		# escreve rede predefinida atual para interface (seção) atual
+		# somente se estiver entre as redes ao alcance da interface
+		for rede_disp in ${REDES[@]}; do
+			[[ "$rede_disp" = "$rede_def" ]] && escr_item "[ $i ]. $rede_def"
+		done
 	done
 
 	# dá outras opções de escolha ao usuário
 	escr_subsecao "Selecionar outras opções"
-	escr_item "[ $i ]. Apagar todas predefinições"
+	escr_item "[ $i ]. Apagar predefinições"
 	escr_item "[ $((++i)) ]. Mostrar mais opções"
 	escr_item "[ $((++i)) ]. Sair"
 
@@ -301,49 +315,110 @@ menu_predefinicoes(){
 
 # ------| DELETA PREDEFINIÇÕES DE INTERFACES E REDES |-----------------------------------
 apagar_predefinicoes(){
-	escr_secao "APAGAR PREDEFINIÇÕES"
-	escr_subsecao "Apagar todas as redes já configuradas? (vazio = n)"
-	escr_item "[ s ]. Sim"
-	escr_item "[ n ]. Não, cancelar"
+	escr_secao "APAGAR PREDEFINIÇÕES ( VAZIO = 0 )"
+	local subsecao=''
+	local i
+
+	# itera array PREDEFINICOES
+	for ((i=0; i < ${#PREDEFINICOES[@]}; i=i+1)); do
+
+		# separa cada predefinição para mostrar no menu
+		local def=${PREDEFINICOES[$i]}
+		local rede_def=$(cut -d":" -f2 <<< $def)
+		local iface_def=$(cut -d":" -f1 <<< $def)
+
+		# só escreve uma nova subseção e escaneia interface se for diferente da anterior
+		if [[ "$iface_def" != "$subsecao" ]]; then
+			escr_subsecao "Redes configuradas para $iface"
+			subsecao="$iface_def"
+		fi
+		escr_item "[ $i ]. $rede_def"
+
+	done
+
+	# dá outras opções de escolha ao usuário
+	escr_subsecao "Selecionar outras opções"
+	escr_item "[ $((++i)) ]. Apagar todas predefinições"
+	escr_item "[ $((++i)) ]. Voltar"
 
 	# variável que armazena valor da opção escolhida no menu
 	local opcao=-1
-	while [[ "$opcao" != "s" && "$opcao" != "n" ]]; do
+
+	# só sai do loop quando ''opcao'' estiver entre 0 e o nº de voltar, ou se ''opcao'' for vazio
+	while [[ 10#${opcao/*[a-zA-Z]*/-1} -lt 0 || 10#${opcao/*[a-zA-Z]*/-1} -gt $i ]]; do
 		escr_item "> "
 		read opcao
 	done
 
+	local predef_num=${#PREDEFINICOES[@]}
+	local apagar_todas=$((predef_num+1))
+	local voltar=$((predef_num+2))
 	local controle=0
 
-	if [[ "$opcao" = "s" ]]; then
-		local tempRedes
-		local i=0
+	case ${opcao:-0} in
+		[0-$predef_num])
+			# separa cada predefinição em uma variavel de rede e interface
+			local def=${PREDEFINICOES[$opcao]}
+			local rede_def=$(cut -d":" -f2 <<< $def)
+			rede_def='"'$rede_def'"'
+			local iface_def=$(cut -d":" -f1 <<< $def)
+			local arquivo="$WPA_CONF_DIR/wpa_supplicant_$iface.conf"
+			local achou=1
+			local inicio=0
+			local final=0
+			local i=0
+			# lê arquivo de configuração da interface para apagar o bloco de configuração da rede
+			while IFS= read -r linha || [[ -n "$linha" ]]; do
+				i=$((i+1))
+				[[ "$linha" = "network={" ]] && inicio=$i
+				[[ "$linha" = "	ssid=$rede_def" && $i -eq $((inicio+1)) ]] && achou=0
+				[[ "$linha" = "}" && $achou -eq 0 ]] && final=$i && break
+			done < "$arquivo"
+			[[ $achou -eq 0 ]] && sed -i "$inicio","$final"d "$arquivo"
+			controle=$achou
+			;;
 
-		# itera cada nome de interface de rede sem-fio achado no sistema
-		for iface in ${INTERFACES[@]}; do
-
-			# itera cada arquivo do diretório abaixo especificado
-			for arq in /etc/wpa_supplicant/*; do
-
-				# apaga arquivo wpa_supplicant_nome-interface.conf para a interface do loop
-				if [[ ${arq##*/} = "wpa_supplicant_$iface.conf" ]]; then
-					( ! rm $arq ) && controle=1
-				fi
+		$apagar_todas)
+			escr_subsecao "Apagar todas as redes já configuradas? (vazio = n)"
+			escr_item "[ s ]. Sim"
+			escr_item "[ n ]. Não, cancelar"
+			# variável que armazena valor da opção de confirmação
+			local opcao=-1
+			while [[ "$opcao" != "s" && "$opcao" != "n" ]]; do
+				escr_item "> "
+				read opcao
 			done
-		done
-	else
-		controle=2
-	fi
+			if [[ "$opcao" = "s" ]]; then
+				local tempRedes
+				local i=0
+				# itera cada nome de interface de rede sem-fio achado no sistema
+				for iface in ${INTERFACES[@]}; do
+					# itera cada arquivo do diretório abaixo especificado
+					for arq in "$WPA_CONF_DIR"/*; do
+						# apaga arquivo wpa_supplicant_nome-interface.conf para a interface do loop
+						if [[ ${arq##*/} = "wpa_supplicant_$iface.conf" ]]; then
+							( ! rm $arq ) && controle=1
+						fi
+					done
+				done
+			else
+				controle=2
+			fi
+			;;
 
-	# 0 = arquivos removidos com sucesso
-	# 1 = algum arquivo não pode ser removido
-	# 2 = ação cancelada
-	case $control in
-		0) escr_item "Configurações removidas com sucesso!" ;;
-		1) escr_item "Um ou mais arquivos não puderam ser removidos." ;;
-		2) escr_item "Ação cancelada!" ;;
+		$voltar)
+			controle=3
+			;;
 	esac
-	return $controle
+
+	# 0=sucesso | # 1=erro | # 2=cancelada | # 3=voltar
+	case $controle in
+		0) escr_item "Configurações removidas com sucesso!" ;;
+		1) escr_item "Uma ou mais configurções não puderam ser removidas." ;;
+		2) escr_item "Ação cancelada!" ;;
+		3) escr_item "Voltando ao menu principal." ;;
+	esac
+	sleep 2
 }
 
 # -----| Checa se arquivo wpa_supplicant_interface.conf |------------------------------------------------
@@ -377,8 +452,8 @@ verifica_wpa_conf(){
 	# Se tal rede não exister, inicia-se o wpa_passphrase para configurar a senha
 	# da rede, e um novo bloco de configuração é criado
 	if [[ -z "$rede" ]]; then
-		( ! wpa_passphrase "$1"  >> "$WPA_CONF_DIR/wpa_supplicant_$2.conf" ) && return 1
-		sed -i /#psk/d "$WPA_CONF_DIR/wpa_supplicant_$2.conf"
+		( ! wpa_passphrase "$1"  >> "$wpaConf" ) && return 1
+		sed -i /#psk/d "$wpaConf"
 	fi
 	return 0
 }
@@ -403,7 +478,7 @@ conectar_rede(){
 	if [[ $wpa_conf -eq 0 ]]; then
 
 		# iniciar wpa_supplicant
-		inicia_wpa_supplicant $2
+		inicia_wpa_supplicant "$2"
 
 		# pega as redes listadas nos blocos do arquivo de configuração
 		local list_networks=$(wpa_cli -i "$2" <<< $'list_networks\nquit')
@@ -412,7 +487,7 @@ conectar_rede(){
 		local id_network=$(grep -i "$1" <<< "$list_networks" | cut -f1)
 
 		# seleciona a rede escolhida
-		wpa_cli -i $2 <<< $'select_network $id_network'
+		wpa_cli -i $2 <<< $"select_network $id_network"
 
 		# aguarda 6 segundos enquando o wpa_cli tenta se autenticar
 		local i=0
@@ -430,7 +505,7 @@ conectar_rede(){
 			# aguarda 6 segundos enquando o dhclient solicita um IP
 			local i=0
 			escr_item "Solicitando endereço IP"
-			killall dhclient
+			dhclient -r
 			dhclient "$2"
 			for i in {0..5}; do
 				echo -n " ."
@@ -513,13 +588,12 @@ menu_principal(){
 		esac
 	fi
 
-	conectar $REDE $INTERFACE
+	conectar_rede $REDE $INTERFACE
 	[[ $? -eq 1 ]] && escr_item "Algo deu errado..." && menu_principal
 }
 
 menu_principal
 
 # AFAZER:
-# - implementar escolha de apagar somente uma predefinição selecionada
-# - em iniciar_wpa() verficar processos por interfaces
-# - em conectar() verificar se rede está ao alance
+# - mensagem de aguardo ao buscar redes para uma interface
+# - verificar conexão com redes predefinidas
